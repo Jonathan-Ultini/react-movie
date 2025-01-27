ac1675d7f057452d66633d7c58f57bf398a3f4a7
Jonathan-Ultini <jonathanultini@gmail.com>
2025-01-27 12:51:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+
+
+const Card = ({title}) => {
+const [hasLiked, setHasLiked] = useState(false)
+
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? '❤️' : '🤍'}
+      </button>
+    </div>
+  )
+}
+
+
+const App = () => {
+
+  return (
+    <div className="card-container">
+      <Card title="Star wars"/>
+      <Card title="Avatar"/>
+      <Card title="The Lion King"/>
+    </div>
+    
+  )
+}
+
+export default App
