423b186fd1e1051deebe04bca10cf02ee9519d4c
Jonathan-Ultini <jonathanultini@gmail.com>
2025-01-29 00:22:49 +0100

    install tailwindcss and restart project

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-
-
-const Card = ({title}) => {
-const [hasLiked, setHasLiked] = useState(false)
-
-  return (
-    <div className="card">
-      <h2>{title}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? '❤️' : '🤍'}
-      </button>
-    </div>
-  )
-}
-
+import React from 'react'
 
 const App = () => {
-
   return (
-    <div className="card-container">
-      <Card title="Star wars"/>
-      <Card title="Avatar"/>
-      <Card title="The Lion King"/>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello World!
+      </h1>
     </div>
-    
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  //da togliere strict mode per duplicazione prima di development
   <StrictMode>
     <App />
   </StrictMode>,
