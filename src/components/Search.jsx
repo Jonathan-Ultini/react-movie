9c26e4f3d177a8d29be6092dbbe7b35cbfad031e
Jonathan-Ultini <jonathanultini@gmail.com>
2025-01-29 01:12:39 +0100

    add search input in header

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Search = () => {
+const Search = ({ searchTerm, setSearchTerm }) => {
   return (
-    <div>Search</div>
+    <div className='search'>
+      <div>
+        <img src='search.svg' alt='search' />
+
+        <input
+          type='text'
+          placeholder='Search through thousands of movies'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
   )
 }
-
 export default Search
+
+//create new state in main and pass like a props to search, props read only
