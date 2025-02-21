f7fefde36b55de69bba40a012be17f57b0891364
Jonathan-Ultini <jonathanultini@gmail.com>
2025-02-21 19:31:12 +0100

    fix and set route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react';
-import Search from './components/search'
+import Search from './components/Search'
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
